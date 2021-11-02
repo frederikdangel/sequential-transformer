5b28d0c4a5506f697c7b3476afebfec514f08388
Frederik Dangel <frederik.dangel@inovex.de>
2021-11-02 15:18:18 +0100

    size
    down sizing

--- experiments/feedback/enwik8.sh
+++ experiments/feedback/enwik8.sh
@@ -10,12 +10,12 @@
 bash ./data/get_enwik8.sh
 
 # Number of GPUs available on your machine
-ngpus=2
+ngpus=1
 
 # If you run out of GPU memory, use --split-batch.
 
 python -m torch.distributed.launch --nproc_per_node=$ngpus main.py \
-    --data ./data/enwik8 --nepochs 100 --nbatches 1000 --batch-sz 1024 --test-batch-sz 128 \
+    --data ./data/enwik8 --nepochs 10 --nbatches 100 --batch-sz 512 --test-batch-sz 64 \
     --feedback --adapt-span --adapt-span-loss 0.0000005 --pre-norm \
-    --hid-sz 512 --inner-hid-sz 4096 --mem-sz 128 --nlayers 12 --attn-lim 8192 --nheads 8 --head-dim 128 \
+    --hid-sz 256 --inner-hid-sz 2048 --mem-sz 64 --nlayers 12 --attn-lim 8192 --nheads 8 --head-dim 128 \
     --lr 0.0015 --momentum 0 --dropout 0.5 --optim adam --lr-warmup 8000 --grad-clip 0.1 --checkpoint-freq 25
